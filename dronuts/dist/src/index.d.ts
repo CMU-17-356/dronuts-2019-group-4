ed22778fa130ba2bf1f88297b8bac59dad97878c
Benji Fishbaum <benjifishbaum@Benjis-MBP.local>
2019-01-29 15:21:44 -0500

    Got loopback working with a homepage, run 'npm start' from dronuts folder to see it work

---
+++ dronuts/dist/src/index.d.ts
@@ -0,0 +1,4 @@
+import { DronutsApplication } from './application';
+import { ApplicationConfig } from '@loopback/core';
+export { DronutsApplication };
+export declare function main(options?: ApplicationConfig): Promise<DronutsApplication>;
