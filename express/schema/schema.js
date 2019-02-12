d4cd590b400e6a16159c8234cb89117c942365cd
Michael Chang <machang@andrew.cmu.edu>
2019-02-12 00:30:45 -0500

    added schema tests

---
+++ express/schema/schema.js
@@ -0,0 +1,46 @@
+const BaseJoi = require('joi');
+const Extension = require('joi-date-extensions');
+const Joi = BaseJoi.extend(Extension);
+
+const userSchema = Joi.object({
+		email: Joi.string().email().required(),
+		username: Joi.string().required(),
+		isCustomer: Joi.number().integer().min(0).max(1),
+		password: Joi.string().required(), // salted, hashed password
+		birthdate: Joi.date().format('MM-DD-YYYY').max('01-01-2001').required(),
+		addresses: Joi.array().items(Joi.object({
+			number: Joi.number().integer().positive().required(),
+			streetname: Joi.string().required(),
+			city: Joi.string().required(),
+			state: Joi.string().required(),
+			zipcode: Joi.string().regex(/^\d{5}(?:[-\s]\d{4})?$/).required()
+		})),
+})
+
+const droneSchema = Joi.object({
+	id: Joi.string().required(),
+	dateAdded: Joi.date().format('MM-DD-YYYY').required(),
+	deliveries: Joi.number().integer().positive()
+})
+
+const orderSchema = Joi.object({
+	address: Joi.object({
+			number: Joi.number().integer().positive().required(),
+			streetname: Joi.string().required(),
+			city: Joi.string().required(),
+			state: Joi.string().required(),
+			zipcode: Joi.string().regex(/^\d{5}(?:[-\s]\d{4})?$/).required()
+		}).required(),
+	donuts: Joi.array().items(Joi.string()).required(),
+    price_dollars: Joi.number().integer().positive(),
+    price_cents: Joi.number().integer().min(0).max(99),
+	user: Joi.string(), // username of the user ordering
+	timeOrdered: Joi.number().positive().integer().required().strict() // time in milliseconds
+})
+
+module.exports = {
+	drone: droneSchema,
+	user: userSchema,
+	order: orderSchema,
+	Joi: Joi
+}
