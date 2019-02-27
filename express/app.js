c289ffea371315d9f735241d996c456a2206e5b2
Saransh Agarwal <saransh@Saranshs-MBP.fios-router.home>
2019-02-26 23:11:54 -0500

    added google log in option, gets email and other basic info from it

--- express/app.js
+++ express/app.js
@@ -29,9 +29,60 @@ app.get('/message', (req, res) => {
 
 app.use('/api',apiRouter)
 
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: "849846779398-lv5g3r773e8pkss1vp02t08vgrhn0g9d.apps.googleusercontent.com",
+    clientSecret: "mbvJWMDqVjcY-B_0vCUnG2Ck",
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      var user = {
+        email: profile.emails[0].value,
+        name: profile.displayName,
+        token: accessToken
+    };
+      cb(null, user);
+  }));
+
+
+// Configure Passport authenticated session persistence.
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(user, cb) {
+  /*User.findById(id, function(err, user) {
+            cb(err, user);
+        });*/
+    cb(null, user);
+});
+
+
+
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', "email"] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: "/", session:false }),
+  function(req, res) {
+    var token = req.user.token;
+    res.redirect('http://localhost:3000?token='+token);
+  });
+
+
 mongoUtil.startMongo().then(() => {
 	database = mongoUtil.getDatabase()
 	app.listen(port, () => console.log("listening"))
 })
+
+
 
 
