c8193208c4c103411a82133c375529d241c1c923
Michael Chang <machang@andrew.cmu.edu>
2019-03-21 01:40:34 -0400

    add back to frontend cuz backend isnt wokring

--- client/src/EmployeePage.js
+++ client/src/EmployeePage.js
@@ -48,23 +48,35 @@ class EmployeePage extends Component {
 
      // Add to Cart
     deleteItem(id) {
+         var droneID = Number(prompt("Specify drone:", "Drone Options are " + this.state.drones));
+         const url =
+              'http://drones.17-356.isri.cmu.edu/api/drones/' + droneID;
+         console.log(droneID);
+         console.log(this.state.drones);
+         console.log(this.state.drones.indexOf(droneID));
+         if (this.state.drones.indexOf(droneID) >= 0) {
+            axios.get(url).then(response => {
+              console.log(response.data)
+              if (response.data.current_delivery.status != "in_route"){
+                    var order = this.state.orders.filter(el => el.orderID == id )[0];
+                    console.log(order);
 
-        axios.get("api/getFreeDrone").then(response => {
-            var order = this.state.orders.filter(el => el.orderID == id )[0];
-            console.log(order);
+                    axios.put('http://drones.17-356.isri.cmu.edu/api/drones/' + droneID + '/send', {
+                    "lat": order.latitude,
+                    "lon": order.longitude,
+                    }).then(response => {
+                        this.setState(prevState => ({
+                            orders: prevState.orders.filter(el => el.orderID != id )
+                            }));
+                    });
 
-            axios.post("api/fillOrder", {
-            "lat": order.latitude,
-            "lon": order.longitude,
-            "droneID": response.droneID,
-            "orderID": order.orderID,
-            }).then(response => {
-                this.setState(prevState => ({
-                    orders: prevState.orders.filter(el => el.orderID != id )
-                    }));
+              }else{
+                alert("Drone Already in Route");
+              }
             });
-
-          });
+        }else {
+           alert("Invalid Drone Id");
+        }
 
         console.log(id);
 
